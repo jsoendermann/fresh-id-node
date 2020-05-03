b3c39cca3279ef38c38cf346403c00c001dc4540
Jan Soendermann <jan.soendermann+git@gmail.com>
2020-05-03 16:43:41 +0100

    Initial implementation

---
+++ index.test.ts
@@ -0,0 +1,22 @@
+import freshId from '.'
+
+describe('fresh-id-node', () => {
+    it('should create correct ids', () => {
+        expect(freshId(20)).toMatch(/^[a-zA-Z0-9]{20}$/)
+        expect(freshId(100)).toMatch(/^[a-zA-Z0-9]{100}$/)
+        expect(freshId()).toMatch(/^[a-zA-Z0-9]{15}$/)
+    })
+
+    it('should log a warning when creating ids with < 15 chars.', () => {
+        const warn = jest.spyOn(console, 'warn').mockImplementation(jest.fn())
+
+        freshId(10)
+
+        expect(warn).toHaveBeenCalledTimes(1)
+        expect(warn).toHaveBeenLastCalledWith('[fresh-id-node] ids shorter than 15 characters are not recommended.')
+    })
+
+    it('should not create empty ids', () => {
+        expect(() => freshId(0)).toThrow(new Error('Ids must be at least one character long.'))
+    })
+})
---
+++ index.ts
@@ -0,0 +1,33 @@
+let crypto
+try {
+    crypto = require('crypto')
+} catch (err) {
+    console.error('[fresh-id-node] crypto module not available.')
+    throw err
+}
+const { randomBytes } = crypto
+
+const ALPHANUM = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz' + '0123456789'
+
+const idGenerator = (length = 15) => {
+    if (length < 1) {
+        throw new Error('[fresh-id-node] ids must be at least one character long.')
+    }
+    if (length < 15) {
+        console.warn('[fresh-id-node] ids shorter than 15 characters are not recommended.')
+    }
+
+    let id = ''
+
+    const random = randomBytes(length)
+
+    let cursor = 0
+    for (let i = 0; i < length; i += 1) {
+        cursor += random[i]
+        id += ALPHANUM[cursor % ALPHANUM.length]
+    }
+
+    return id
+}
+
+export default idGenerator
